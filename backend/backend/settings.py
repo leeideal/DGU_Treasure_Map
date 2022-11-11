3ae2445e1cd2f56e9f50cfa992119fab2b752822
ustar1210 <ays991210@naver.com>
2022-11-11 18:43:05 +0900

    B001 - model, serializer 작성

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'post'
+    'post',
 
     # 설치한 라이브러리들
     'rest_framework',
