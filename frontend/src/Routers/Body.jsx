7c55a15a93d1561b1778ab35ee2de1d75b51a1ee
Lee Sangdon <88819145+leeideal@users.noreply.github.com>
2022-11-13 22:35:19 +0900

    leeideal-F001-Header

---
+++ frontend/src/Routers/Body.jsx
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+    width: 100%;
+    background-color: #C7E2DD;
+`
+
+function Body() {
+    return (
+        <Container>
+            
+        </Container>
+    )
+}
+export default Body;
