41747c6955d229a998246fd914268d9a34ad8531
0seoYun <yys020819@gmail.com>
2022-11-14 22:15:29 +0900

    B 카테

--- backend/post/views.py
+++ backend/post/views.py
@@ -33,19 +33,33 @@ def get_place(request, id):
 
 @api_view(['GET'])
 def get_category(request, id):
+    place_id = Place.objects.get(id=id)
     category_id = Category.objects.get(id=id)
-    facilities = Facility.objects.filter(place=category_id)
-    serializer = FacilitySerializer(facilities, many=True)
+    facilities = Facility.objects.filter(category=category_id)
 
-    # facility_set = {
-    #     "name" = serializer.data['name'],
-    #     "category",
-    #     "id",
-    #     "time",
-    #     "img",
-    #     "phone",
-    #     "use",
-    #     "where",
-    #     "floor"
-    # }
-    return Response(data=serializer.data)
+    send_place = []
+    for i in facilities:
+        place_name = i.place.name
+        if place_name not in send_place:
+            send_place.append(place_name)
+
+    send_facility = []
+    
+    for i in facilities:
+        place_name = place_id.name
+        category_name = i.category.name
+
+        # # facility_set = {
+        # #     "name":i.name,
+        # #     "category":category_name,
+        # #     "id":i.id,
+        # #     "time":,
+        # #     "img":,
+        # #     "phone":,
+        # #     "use":,
+        # #     "where":place_name,
+        # #     "floor":
+        # # }
+        # send_facility.append(facility_set)
+
+    return Response(data=send_facility)
