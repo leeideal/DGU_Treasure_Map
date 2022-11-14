2a303ee1731f13bd7e9e26c8a96c594817940bbe
kmjenny <kjn3008@gmail.com>
2022-11-14 20:05:17 +0900

    b004

--- backend/post/views.py
+++ backend/post/views.py
@@ -10,17 +10,23 @@ from .serializers import FacilitySerializer,PlaceSerializer,CategorySerializer
 def get_place(request, id):
     place_id = Place.objects.get(id=id)
     facilities = Facility.objects.filter(place=place_id)
-    serializer = FacilitySerializer(facilities, many=True)
 
-    # facility_set = {
-    #     "name" = serializer.data['name'],
-    #     "category",
-    #     "id",
-    #     "time",
-    #     "img",
-    #     "phone",
-    #     "use",
-    #     "where",
-    #     "floor"
-    # }
-    return Response(data=serializer.data)
+    send_facility = []
+    for i in facilities:
+        place_name = place_id.name
+        category_name = i.category.name
+
+        # # facility_set = {
+        # #     "name":i.name,
+        # #     "category":category_name,
+        # #     "id":i.id,
+        # #     "time":,
+        # #     "img":,
+        # #     "phone":,
+        # #     "use":,
+        # #     "where":place_name,
+        # #     "floor":
+        # # }
+        # send_facility.append(facility_set)
+
+    return Response(data=send_facility)
