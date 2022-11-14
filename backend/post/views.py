18a09d11b637e1615c8dcd6c780f2ee509bd9a7f
kmjenny <kjn3008@gmail.com>
2022-11-14 19:38:42 +0900

    b업뎃

--- backend/post/views.py
+++ backend/post/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
 
+from .models import *
+from .serializers import FacilitySerializer,PlaceSerializer,CategorySerializer
 # Create your views here.
+
+@api_view(['GET'])
+def get_place(request, id):
+    place_id = Place.objects.get(id=id)
+    facilities = Facility.objects.filter(place=place_id)
+    serializer = FacilitySerializer(facilities, many=True)
+
+    # facility_set = {
+    #     "name" = serializer.data['name'],
+    #     "category",
+    #     "id",
+    #     "time",
+    #     "img",
+    #     "phone",
+    #     "use",
+    #     "where",
+    #     "floor"
+    # }
+    return Response(data=serializer.data)
